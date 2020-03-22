f393f24fa8351ebc0e36394efeab8b5e3facae71
Brandon Fong <55638760+BrandonMFong@users.noreply.github.com>
2020-03-22 00:21:14 -0700

    [Issue 1] Fix was DCT_CbCr was a uint8 matrix.  Changed to double data type

--- SaveOutputs.m
+++ SaveOutputs.m
@@ -1,5 +1,5 @@
 % Saving into ./Outputs directory
-var = jsondecode(fileread('Save.json'));
+savevar = jsondecode(fileread('Save.json'));
 save("./Outputs/PSNR_Y.txt", "PSNR_Y", '-ascii')
 save("./Outputs/PSNR_Cb.txt", "PSNR_Cb", '-ascii')
 save("./Outputs/PSNR_Cr.txt", "PSNR_Cr", '-ascii')
@@ -10,15 +10,9 @@ for ii = 1:size(Inverse_QDCT_Cb,1)
     fprintf(fid,'\n');
 end
 fclose(fid)
-fid = fopen('./Outputs/Inverse_QDCT_Cb.csv','wt');
-for ii = 1:size(Inverse_QDCT_Cb,1)
-    fprintf(fid,'%g\t',Inverse_QDCT_Cb(ii,:));
-    fprintf(fid,'\n');
-end
-fclose(fid)
 
 imwrite(DCTBlock1,'./Outputs/DCTBlock1.jpg')
-imwrite(DCTBlock2,'./Outputs/DCTBlock2.tif')
-imwrite(NewImgYCbCr,'./Outputs/NewImgYCbCr.tif')
-imwrite(NewImgRGB,'./Outputs/NewImgRGB.tif')
-imwrite(DiffImg,'./Outputs/DiffImg.tif')
+imwrite(DCTBlock2,'./Outputs/DCTBlock2.jpg')
+imwrite(NewImgYCbCr,'./Outputs/NewImgYCbCr.jpg')
+imwrite(NewImgRGB,'./Outputs/NewImgRGB.jpg')
+imwrite(DiffImg,'./Outputs/DiffImg.jpg')
