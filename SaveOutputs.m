37a74800f67cea8252574fe4afbf1bd622d3c7ad
Brandon Fong <55638760+BrandonMFong@users.noreply.github.com>
2020-03-22 13:38:28 -0700

    Editting Save.json to save outputs to files

--- SaveOutputs.m
+++ SaveOutputs.m
@@ -3,18 +3,28 @@
 %   VariableName: Name of the variable in the workspace
 %   Path: Path to save to.  Can be full path or path start from this project folder
 %   Type: Constant, Matrix, or Image.  Must specify or variable will not be saved
+if ~exist('Outputs', 'dir')
+        mkdir('Outputs')
+end 
 savevar = jsondecode(fileread('Save.json'));
 for i = 1:size(savevar.Save,1)
     if savevar.Save(i).Type == "Constant"
         save(savevar.Save(i).Path,savevar.Save(i).VariableName,'-ascii')
     elseif savevar.Save(i).Type == "Matrix"
-        file = fopen(savevar.Save(i).Path,'wt');
-        eval(['val = ',savevar.Save(i).VariableName,';']);
-        for i = 1:size(val,1)
-            fprintf(file,'%g,',val(i,:));
-            fprintf(file,'\n');
+        % if ~isfile(savevar.Save(i).Path)
+        %     fprintf('%s does not exist\n\n', savevar.Save(i).Path)
+        % end 
+        try
+            file = fopen(savevar.Save(i).Path,'wt');
+            eval(['val = ',savevar.Save(i).VariableName,';']);
+            for i = 1:size(val,1)
+                fprintf(file,'%g,',val(i,:));
+                fprintf(file,'\n');
+            end
+            fclose(file);
+        catch exception
+            throw(exception)
         end
-        fclose(file);
     elseif savevar.Save(i).Type == "Image"
         eval(['val = ',savevar.Save(i).VariableName,';']);
         imwrite(val,savevar.Save(i).Path);
@@ -22,17 +32,3 @@ for i = 1:size(savevar.Save,1)
         fprintf('%s not saved into file, please check Save.json if Type is specified.', savevar.Save(i).VariableName); 
     end
 end
-
-% file = fopen('./Outputs/Inverse_QDCT_Cb.csv','wt');
-% eval(['val = ','Inverse_QDCT_Cb',';']);
-% for ii = 1:size(val,1)
-%     fprintf(file,'%g,',val(ii,:));
-%     fprintf(file,'\n');
-% end
-% fclose(file)
-
-% imwrite(DCTBlock1,'./Outputs/DCTBlock1.jpg')
-% imwrite(DCTBlock2,'./Outputs/DCTBlock2.jpg')
-% imwrite(NewImgYCbCr,'./Outputs/NewImgYCbCr.jpg')
-% imwrite(NewImgRGB,'./Outputs/NewImgRGB.jpg')
-% imwrite(DiffImg,'./Outputs/DiffImg.jpg')
