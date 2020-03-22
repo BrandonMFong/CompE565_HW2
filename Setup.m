4640b7f9358ff53ca8f9d277d58cd53805ee584c
Brandon Fong <55638760+BrandonMFong@users.noreply.github.com>
2020-03-22 02:20:13 -0700

    Setup.m makes the output directory but all files in the directory is ignored

--- Setup.m
+++ Setup.m
@@ -3,14 +3,9 @@ clear
 % Must have Matlab R2016b and higher
 % If not, comment out Part B and included Part A
 try 
-
-    % A
-    % Paths for project
-    % addpath './Questions';
-    % addpath './Questions/Misc';
-    % addpath './QuantizationMatrices';
-
-    % B
+    if ~exist('Outputs', 'dir')
+        mkdir('Outputs')
+     end 
     var = jsondecode(fileread('Paths.json'));
     [rows, columns] = size(var.Paths);
     for r = 1:rows
